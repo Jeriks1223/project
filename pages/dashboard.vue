f9f5329e0725228b3cccf40940c60e7a5ec20235
jeriks1223 <jerectamz@gmail.com>
2022-10-22 01:37:06 +0800

    commit

---
+++ pages/dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+    <Dashboard/>
+  </template>
+  
+  <script>
+ 
+import Dashboard from '../components/Dashboard.vue';
+  export default {
+      name: "Dashboard",
+      components: { Dashboard }
+  }
+  </script>
+  
